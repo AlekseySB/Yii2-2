5bbfcd209b198acf82e10050302177d3259d7dc2
AlekseySB <abilyakov@yandex.ru>
2019-09-11 15:41:59 +0300

    L3

---
+++ backend/components/chat/widget/ChatAsset.php
@@ -0,0 +1,16 @@
+    coon: null,
+<?php
+
+
+namespace backend\components\chat\widget;
+
+
+use yii\web\AssetBundle;
+
+class ChatAsset extends AssetBundle
+{
+    public $sourcePath = '@backend/components/chat/widget/source';
+    public $js = [
+        'js/chat.js'
+    ];
+}
